8cbcc0a0ccc04f0c83689314ebff5c8a138a09c1
Guilherme Pressutto <gpressutto5@gmail.com>
2018-05-14 14:46:37 -0300

    Defining recipients

---
+++ src/Slack.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Pressutto\LaravelSlack;
+
+use Illuminate\Support\Collection;
+
+class Slack
+{
+    /**
+     * @var string[]
+     */
+    private $recipients = [];
+
+    /**
+     * Set the recipients of the message.
+     *
+     * @param  object|array|string $recipient
+     *
+     * @return $this
+     */
+    public function to($recipient)
+    {
+        if ($recipient instanceof Collection) {
+            $recipient = $recipient->toArray();
+        }
+
+        $recipients = is_array($recipient) ? $recipient : func_get_args();
+
+        return $this->setRecipient($recipients);
+    }
+
+    /**
+     * Set the recipients of the message.
+     * All recipients are stored internally as ['#general', '@user', '@another_user']
+     *
+     * @param  object|array|string $recipient
+     *
+     * @return $this
+     */
+    private function setRecipient($recipient)
+    {
+        $this->recipients = $recipient;
+
+        return $this;
+    }
+}
---
+++ tests/RecipientsTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Tests;
+
+use Pressutto\LaravelSlack\Slack;
+use ReflectionObject;
+
+class RecipientsTest extends TestCase
+{
+    public function testSettingSingleChannelStringRecipient()
+    {
+        $slack = (new Slack())->to('#general');
+
+        $recipientsArray = $this->getPrivatePropertyValueFromObject('recipients', $slack);
+
+        $this->assertEquals(['#general'], $recipientsArray);
+    }
+
+    public function testSettingSingleUserStringRecipient()
+    {
+        $slack = (new Slack())->to('@user');
+
+        $recipientsArray = $this->getPrivatePropertyValueFromObject('recipients', $slack);
+
+        $this->assertEquals(['@user'], $recipientsArray);
+    }
+
+    public function testSettingMultipleUsersStringRecipient()
+    {
+        $slack = (new Slack())->to('@user1', '@user2', '@user3');
+
+        $recipientsArray = $this->getPrivatePropertyValueFromObject('recipients', $slack);
+
+        $this->assertEquals(['@user1', '@user2', '@user3'], $recipientsArray);
+    }
+
+    public function testSettingSingleUserArrayRecipient()
+    {
+        $slack = (new Slack())->to(['@user']);
+
+        $recipientsArray = $this->getPrivatePropertyValueFromObject('recipients', $slack);
+
+        $this->assertEquals(['@user'], $recipientsArray);
+    }
+
+    public function testSettingMultipleUsersArrayRecipient()
+    {
+        $slack = (new Slack())->to(['@user1', '@user2', '@user3']);
+
+        $recipientsArray = $this->getPrivatePropertyValueFromObject('recipients', $slack);
+
+        $this->assertEquals(['@user1', '@user2', '@user3'], $recipientsArray);
+    }
+
+    public function testSettingSingleUserCollectionRecipient()
+    {
+        $slack = (new Slack())->to(collect(['@user']));
+
+        $recipientsArray = $this->getPrivatePropertyValueFromObject('recipients', $slack);
+
+        $this->assertEquals(['@user'], $recipientsArray);
+    }
+
+    public function testSettingMultipleUsersCollectionRecipient()
+    {
+        $slack = (new Slack())->to(collect(['@user1', '@user2', '@user3']));
+
+        $recipientsArray = $this->getPrivatePropertyValueFromObject('recipients', $slack);
+
+        $this->assertEquals(['@user1', '@user2', '@user3'], $recipientsArray);
+    }
+
+    /**
+     * @param string $propertyName
+     * @param object $object
+     *
+     * @return mixed
+     */
+    private function getPrivatePropertyValueFromObject(string $propertyName, $object)
+    {
+        $reflection = new ReflectionObject($object);
+        $property   = $reflection->getProperty($propertyName);
+        $property->setAccessible(true);
+
+        return $property->getValue($object);
+    }
+}
